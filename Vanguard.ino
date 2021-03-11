4192071da0edcd168317d7160f4a73d9afe2b1a7
johnmacrae <jramacrae@gmail.com>
2021-03-11 19:50:30 +0000

    Onewire Pin

--- Vanguard.ino
+++ Vanguard.ino
@@ -12,7 +12,7 @@
 #define BUTTON_PIN 0
 
 // Data wire is plugged into port 2 on the Arduino
-#define ONE_WIRE_BUS 2
+#define ONE_WIRE_BUS 23
 
 // Setup a oneWire instance to communicate with any OneWire devices (not just Maxim/Dallas temperature ICs)
 OneWire oneWire(ONE_WIRE_BUS);
