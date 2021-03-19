cb37e46883a72149dd6203719afc8f0209810bc0
johnmacrae <jramacrae@gmail.com>
2021-03-19 13:54:11 +0000

    Incorporate IOTAppstory

--- Vanguard.ino
+++ Vanguard.ino
@@ -1,16 +1,16 @@
-#include <WiFi.h>
+#define COMPDATE __DATE__ __TIME__
+#define MODEBUTTON 0                                        // Button pin on the esp for selecting modes. D3 for the Wemos!
+#define LOOP_WAIT 60000
+
+#include <IOTAppStory.h>                                    // IotAppStory.com library
+IOTAppStory IAS(COMPDATE, MODEBUTTON);                      // Initialize IotAppStory
+
 #include <WiFiClientSecure.h>
 #include <UniversalTelegramBot.h>
-#include <FS.h>
-#include <SPIFFS.h>
-#include <WiFiManager.h>
-#include "credentials.h"
-// Include the libraries we need
 #include <OneWire.h>
 #include <DallasTemperature.h>
 
-#define BUTTON_PIN 0
-
+//  #define BUTTON_PIN 0
 // Data wire is plugged into port 23 on the ESP32
 #define ONE_WIRE_BUS 23
 
@@ -23,9 +23,29 @@ DallasTemperature sensors(&oneWire);
 // arrays to hold device address
 DeviceAddress insideThermometer;
 
-// Wifi network station credentials
-//#define WIFI_SSID "secret"
-//#define WIFI_PASSWORD "secretpassword"
+// ================================================ EXAMPLE VARS =========================================
+// used in this example to print variables every 10 seconds
+unsigned long printEntry;
+String deviceName = "Vanguard";
+String chipId;
+
+// We want to be able to edit these example variables below from the wifi config manager
+// Currently only char arrays are supported.
+// Use functions like atoi() and atof() to transform the char array to integers or floats
+// Use IAS.dPinConv() to convert Dpin numbers to integers (D6 > 14)
+
+char* lbl         = "Vanguard";
+char* updInt      = "7200";
+char* ledPin      = "2";
+
+char* botToken = "11:aa";
+char* tempCLimit = "25";
+char* vBattLimit = "11.8";
+char* vHouseLimit = "11.8";
+char* sleepTime = "300";
+
+String chat_id2 = "725925511";
+char* chat_id = "-1";
 
 //  ****Telegram BOT Token (Get from Botfather) and define in credentials.txt ****
 
@@ -35,43 +55,35 @@ unsigned long bot_lasttime; // last time messages' scan has been done
 float tempC = 0;
 float vBatt = 0;
 float vHouse = 0;
+long lSleepTime = 0;
 
 float v1 = 0;
 float v2 = 0;
 
-char botToken[60];
-
-char tempCLimit[3] = "25";
-char vBattLimit[5] = "11.8";
-char vHouseLimit[5] = "11.8";
-char sleepTime[5] = "300";
-
-long lSleepTime = 0;
-
-String chat_id2 = "725925511";
-String chat_id = "-1";
-char chatId[20]  = "-1";
-
-//flag for saving data
-bool shouldSaveConfig = false;
-
 WiFiClientSecure secured_client;
-UniversalTelegramBot bot(botToken, secured_client);
+UniversalTelegramBot bot(String(botToken), secured_client);
 
 void handleNewMessages(int numNewMessages)
 {
   Serial.print("handleNewMessages ");
   Serial.println(numNewMessages);
 
-  String answer = "handle message \n";
-  
-  if ((bot.messages[0].chat_id == chat_id2) || (bot.messages[0].chat_id == chat_id))
+  String answer = "Answer message \n";
+
+  Serial.print("chat_id: ");
+  Serial.println(chat_id);
+
+  if ((bot.messages[0].chat_id == chat_id2) || (bot.messages[0].chat_id == String(chat_id)))
   {
+
+    Serial.print("chat_id: ");
+    Serial.println(chat_id);
+
     for (int i = 0; i < numNewMessages; i++)
     {
       telegramMessage &msg = bot.messages[i];
-      Serial.println("Answering message " +  String(i)+ msg.text);
-      answer = "Temperature: " ;
+      Serial.println("Answering message " +  String(i) + msg.text);
+      answer += "Temperature: " ;
       answer.concat( tempC);
       answer.concat("\n");
       answer += "Vbatt: ";
@@ -116,7 +128,6 @@ void sendMessage()
   Serial.println(botToken);
 }
 
-
 void bot_setup()
 {
   const String commands = F("["
@@ -124,198 +135,91 @@ void bot_setup()
                             "{\"command\":\"status\",\"description\":\"Get current status\"}" // no comma on last command
                             "]");
   bot.setMyCommands(commands);
-  //bot.sendMessage("25235518", "Hola amigo!", "Markdown");
-}
 
-void setupSpiffs() {
-  // clean FS, for testing
-  //SPIFFS.format();
-
-  //read configuration from FS json
-  Serial.println("mounting FS...");
-
-  // May need to make it begin(true) first time you are using SPIFFS
-  if (SPIFFS.begin(true)) {
-    Serial.println("mounted file system");
-    if (SPIFFS.exists("/config.json")) {
-      //file exists, reading and loading
-      Serial.println("reading config file");
-      File configFile = SPIFFS.open("/config.json", "r");
-      if (configFile) {
-        Serial.println("opened config file");
-        StaticJsonDocument<512> json;
-        DeserializationError error = deserializeJson(json, configFile);
-        serializeJsonPretty(json, Serial);
-        if (!error) {
-          Serial.println("\nparsed json");
-
-          strcpy(botToken, json["botToken"]);
-          strcpy(tempCLimit, json["tempCLimit"]);
-          strcpy(vBattLimit, json["vBattLimit"]);
-          strcpy(vHouseLimit, json["vHouseLimit"]);
-          strcpy(chatId, json["chatId"]);
-          strcpy(sleepTime, json["sleepTime"]);
-
-        } else {
-          Serial.println("failed to load json config");
-        }
-      }
-    }
-  } else {
-    Serial.println("failed to mount FS");
-  }
-  //end read
-}
-
-void saveConfigCallback () {
-  Serial.println("Should save config");
-  shouldSaveConfig = true;
 }
 
 void setup()
 {
-  Serial.begin(115200);
-  Serial.println();
-  pinMode(BUTTON_PIN, INPUT_PULLUP);
 
-  setupSpiffs();
+  // create a unique deviceName for classroom situations (deviceName-123)
+  chipId      = String(ESP_GETCHIPID);
+  chipId      = "-" + chipId.substring(chipId.length() - 3);
+  deviceName += chipId;
 
-  WiFiManager wm;
+  /* TIP! delete lines below when not used */
+  IAS.preSetDeviceName(deviceName);                         // preset deviceName this is also your MDNS responder: http://virginsoil-123.local
+  IAS.preSetAutoUpdate(false);                            // automaticUpdate (true, false)
+  //IAS.preSetAutoConfig(false);                            // automaticConfig (true, false)
+  /* TIP! Delete Wifi cred. when you publish your App. */
 
-  //set config save notify callback
-  wm.setSaveConfigCallback(saveConfigCallback);
-  /*          strcpy(botToken, json["botToken"]);
-            strcpy(tempCLimit, json["tempCLimit"]);
-            strcpy(vBattLimit, json["vBattLimit"]);
-            strcpy(vHouseLimit, json["vHouseLimit"]);
-  */
-  WiFiManagerParameter custom_bot_token("botToken", "Bot Token", botToken, 60);
-  WiFiManagerParameter custom_tempCLimit("tempCLimit", "Temp Limit (C)", tempCLimit, 4);
-  WiFiManagerParameter custom_batt_limit("vBattLimit", "Veh Batt Limit", vBattLimit, 4);
-  WiFiManagerParameter custom_house_limit("vHouseLimit", "House Batt Limit", vHouseLimit, 4);
-  WiFiManagerParameter custom_chat_id("chatId", "Chat ID", chatId, 20);
-  WiFiManagerParameter custom_sleepTime("sleepTime", "Sleep Time", sleepTime, 5);
+  IAS.addField(updInt, "Interval", 8, 'I');
+  IAS.addField(ledPin, "ledPin", 2, 'P');
+  //  IAS.addField(timeZone, "Timezone", 48, 'Z');
 
+  IAS.addField(botToken, "botToken", 60, 'T');
+  IAS.addField(tempCLimit, "tempCLimit", 25, 'N');
+  IAS.addField(vBattLimit, "vBattLimit", 11.8, 'N');
+  IAS.addField(vHouseLimit, "vHouseLimit", 11.8, 'N');
+  IAS.addField(chat_id, "chat_id", 20, 'T');
+  IAS.addField(ledPin, "sleepTime", 5, 'I');
 
-  //add all your parameters here
-  wm.addParameter(&custom_bot_token);
-  wm.addParameter(&custom_tempCLimit);
-  wm.addParameter(&custom_batt_limit);
-  wm.addParameter(&custom_house_limit);
-  wm.addParameter(&custom_chat_id);
-  wm.addParameter(&custom_sleepTime);
+  // You can configure callback functions that can give feedback to the app user about the current state of the application.
+  // In this example we use serial print to demonstrate the call backs. But you could use leds etc.
 
-  //reset settings - wipe credentials for testing
-  //wm.resetSettings();
-delay (2000);
-  if (digitalRead(BUTTON_PIN) == LOW) {
-    Serial.println("Entering Forced Config Mode");
-    if (!wm.startConfigPortal("Vanguard", "everyday")) {
-      Serial.println("failed to connect and hit timeout");
-      delay(3000);
-      //reset and try again, or maybe put it to deep sleep
-      ESP.restart();
-      delay(5000);
-    }
-  } else {
-    if (!wm.autoConnect("SmarterDisplay", "everyday")) {
-      Serial.println("failed to connect and hit timeout");
-      delay(3000);
-      // if we still have not connected restart and try all over again
-      ESP.restart();
-      delay(5000);
-    }
-  }
+  IAS.onModeButtonShortPress([]() {
+    Serial.println(F(" If mode button is released, I will enter in firmware update mode."));
+    Serial.println(F("*-------------------------------------------------------------------------*"));
+  });
 
-  strcpy(botToken, custom_bot_token.getValue());
-  strcpy(tempCLimit, custom_tempCLimit.getValue());
-  strcpy(vBattLimit, custom_batt_limit.getValue());
-  strcpy(vHouseLimit, custom_house_limit.getValue());
-  strcpy(vBattLimit, custom_batt_limit.getValue());
-  strcpy(chatId, custom_chat_id.getValue());
-  strcpy(sleepTime, custom_sleepTime.getValue());
+  IAS.onModeButtonLongPress([]() {
+    Serial.println(F(" If mode button is released, I will enter in configuration mode."));
+    Serial.println(F("*-------------------------------------------------------------------------*"));
+  });
 
+  IAS.onModeButtonVeryLongPress([]() {
+    Serial.println(F(" If mode button is released, I won't do anything unless you program me to."));
+    Serial.println(F("*-------------------------------------------------------------------------*"));
+    /* TIP! You can use this callback to put your app on it's own configuration mode */
+  });
+
+  IAS.onFirmwareUpdateProgress([](int written, int total) {
+    Serial.print(".");
+  });
+
+  IAS.begin();                                            // Run IOTAppStory
+  IAS.setCallHomeInterval(atoi(updInt));                  // Call home interval in seconds(disabled by default), 0 = off, use 60s only for development. Please change it to at least 2 hours in production
+
+  lSleepTime = atol(sleepTime);
   float ftempLimit = atof(tempCLimit);
   float fBattLimit = atof(vBattLimit);
   float fHouseLimit = atof(vHouseLimit);
-  lSleepTime = atol(sleepTime);
-  chat_id = String(chatId);
-  if (shouldSaveConfig) {
-    Serial.println("saving config");
-    DynamicJsonDocument  json(200);
-    json["botToken"] = botToken;
-    json["tempCLimit"]   = tempCLimit;
-    json["vHouseLimit"]   = vHouseLimit;
-    json["vBattLimit"]   = vBattLimit;
-    json["chatId"]   = chatId;
-    json["sleepTime"]   = sleepTime;
 
-    File configFile = SPIFFS.open("/config.json", "w");
-    if (!configFile) {
-      Serial.println("failed to open config file for writing");
-    }
-
-    serializeJsonPretty(json, Serial);
-    if (serializeJson(json, configFile) == 0) {
-      Serial.println(F("Failed to write to file"));
-    }
-    configFile.close();
-    //end save
-    shouldSaveConfig = false;
-  }
-
-  Serial.print("\nWiFi connected. IP address: ");
-  Serial.println(WiFi.localIP());
+  Serial.print("botToken: ");
+  Serial.println(botToken);
 
   bot.updateToken(String(botToken));
-
-  /*  // attempt to connect to Wifi network:
-    Serial.print("Connecting to Wifi SSID ");
-    Serial.print(WIFI_SSID);
-    WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
-    secured_client.setCACert(TELEGRAM_CERTIFICATE_ROOT); // Add root certificate for api.telegram.org
-    while (WiFi.status() != WL_CONNECTED)
-    {
-      Serial.print(".");
-      delay(500);
-    }
-  */
-  Serial.print("\nWiFi connected. IP address: ");
-  Serial.println(WiFi.localIP());
-  /*
-    Serial.print("Retrieving time: ");
-    configTime(0, 0, "pool.ntp.org"); // get UTC time via NTP
-    time_t now = time(nullptr);
-    while (now < 24 * 3600)
-    {
-      Serial.print(".");
-      delay(100);
-      now = time(nullptr);
-    }
-    Serial.println(now);
-  */
-  Serial.print("ChatID check: ");
-  Serial.println(chatId);
 
   bot_setup();
   temp_setup();
   temp_loop();
   adc_setup();
   adc_loop();
-  /*
-    float ftempLimit = atof(tempCLimit);
-    float fBattLimit = atof(vBattLimit);
-    float fHouseLimit = atof(vHouseLimit);
-  */
-  /*
-    v1 = 10.8;
-    v2 = 12.8;
-    tempC = 24;
-  */
 
-  Serial.println(v1);
-  Serial.println(v2);
-  Serial.println(tempC);
+  Serial.print("V1: ");
+  Serial.print(v1);
+  Serial.print(" Limit: ");
+  Serial.println(fBattLimit);
+
+  Serial.print("V2: ");
+  Serial.print(v2);
+  Serial.print(" Limit: ");
+  Serial.println(fHouseLimit);
+
+  Serial.print("Temp: ");
+  Serial.print(tempC);
+  Serial.print(" Limit: ");
+  Serial.println(ftempLimit);
+
   if ((v1 < fBattLimit) || (v2 < fHouseLimit) || (tempC > ftempLimit))
   {
     Serial.println(fBattLimit);
@@ -323,10 +227,13 @@ delay (2000);
     Serial.println(ftempLimit);
     sendMessage();
   }
+  Serial.println("End setup");
 }
 
 void loop()
 {
+  IAS.loop();   // this routine handles the calling home functionality,
+
   if (millis() - bot_lasttime > BOT_MTBS)
   {
     int numNewMessages = bot.getUpdates(bot.last_message_received + 1);
@@ -340,6 +247,8 @@ void loop()
 
     bot_lasttime = millis();
   }
-  sleep_setup();
-
+  if (millis() > LOOP_WAIT)//Wait for a while for the user to press a button
+  {
+    sleep_setup();
+  }
 }
